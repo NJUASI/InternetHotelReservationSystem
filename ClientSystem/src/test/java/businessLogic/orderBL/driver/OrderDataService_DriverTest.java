47627116790d4201a137cc840924b4c8fe4a60c2
CharlesFeng <fdfengjunjie@126.com>
2016-12-06 00:06:25 +0800

    解决冲突

--- ClientSystem/src/test/java/businessLogic/orderBL/driver/OrderDataService_DriverTest.java
+++ ClientSystem/src/test/java/businessLogic/orderBL/driver/OrderDataService_DriverTest.java
@@ -51,19 +51,15 @@ public class OrderDataService_DriverTest {
 		final OrderState orderState = OrderState.EXECUTED;
 		final RoomType roomType = RoomType.AMBASSADOR;
 		
-<<<<<<< Updated upstream
-=======
-		final OrderGeneralVO createOrderGeneralVO = new OrderGeneralVO("1234567890", "12345678", "thisHotel", 
-				"nanjing", expectExecuteTime, expectLeaveTime, "zhangsan", "15012345678");
-		final OrderVO createOrderVO = new OrderVO(createOrderGeneralVO, 250, roomType, 1, 2, "charles", "15012345678", "no");
->>>>>>> Stashed changes
-		
-		final OrderPO orderPO = new OrderPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200, 200,
-				createTime, checkInTime, checkOutTime, expectExecuteTime, expectLeaveTime, orderState, 
-				roomType, 2, "301", 2, "zhangsan", "13554321234", "no","good");
+
+		final OrderGeneralPO createOrderGeneralVO = new OrderGeneralPO("123456789012", "1234567890", 
+				"12345678", "thisHotel", "nanjing", 200, expectExecuteTime, expectLeaveTime, orderState, 
+				false, "zhangsan", "15012345678");
+		final OrderPO createOrderVO = new OrderPO(createOrderGeneralVO, 250, createTime, checkInTime, checkOutTime, 
+				roomType, 1, "305", 2, "no", 4.5, "good");
 		
 		try {
-			assertEquals(ResultMessage.SUCCESS, driver.orderDataService.createOrder(orderPO));
+			assertEquals(ResultMessage.SUCCESS, driver.orderDataService.createOrder(createOrderVO));
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
