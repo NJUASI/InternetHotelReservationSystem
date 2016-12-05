47892eb7382dc202d29acfb7039b7743f0d442a9
CharlesFeng <fdfengjunjie@126.com>
2016-12-05 19:07:01 +0800

    order测试

--- ClientSystem/src/test/java/businessLogic/orderBL/driver/OrderDataService_DriverTest.java
+++ ClientSystem/src/test/java/businessLogic/orderBL/driver/OrderDataService_DriverTest.java
@@ -10,21 +10,55 @@ import java.util.List;
 import org.junit.Test;
 
 import dataService.orderDataService.OrderDataService_Stub;
+import po.CheckInPO;
+import po.CheckOutPO;
+import po.GuestEvaluationPO;
+import po.HotelEvaluationPO;
 import po.OrderGeneralPO;
 import po.OrderPO;
 import utilities.OrderState;
 import utilities.ResultMessage;
 import utilities.RoomType;
+import vo.OrderGeneralVO;
+import vo.OrderVO;
 
 /**
  * 
  * @author charles
  * lastChangedBy charles
- * updateTime 2016/11/27
+ * updateTime 2016/12/5
  *
  */
 public class OrderDataService_DriverTest {
 
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface getTempPrice
+	 */
+	@Test
+	public void test1() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		final LocalDateTime expectExecuteTime = LocalDateTime.of(2016, 2, 3, 14, 00);
+		final LocalDateTime expectLeaveTime = LocalDateTime.of(2016, 2, 4, 12, 00);
+		final RoomType roomType = RoomType.AMBASSADOR;
+		
+		final OrderGeneralVO createOrderGeneralVO = new OrderGeneralVO("1234567890", "12345678", "thisHotel", 
+				"nanjing", expectExecuteTime, expectLeaveTime);
+		final OrderVO createOrderVO = new OrderVO(createOrderGeneralVO, 250, roomType, 1, 2, "charles", "15012345678", "no");
+		double result = 0;
+		try {
+			result = driver.orderDataService.getTempPrice(new OrderPO(createOrderVO));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		assertEquals(200, result, 0);
+	}
+	
 	/**
 	 * @author charles
 	 * @lastChangedBy charles
@@ -33,23 +67,19 @@ public class OrderDataService_DriverTest {
 	 * test interface createOrder
 	 */
 	@Test
-	public void test1() {
+	public void test2() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
-		
-		final LocalDateTime createTime = LocalDateTime.of(2016, 2, 2, 18, 20);
-		final LocalDateTime checkInTime = LocalDateTime.of(2016, 2, 3, 11, 23);
-		final LocalDateTime checkOutTime = LocalDateTime.of(2016, 2, 4, 10, 58);
+
 		final LocalDateTime expectExecuteTime = LocalDateTime.of(2016, 2, 3, 14, 00);
 		final LocalDateTime expectLeaveTime = LocalDateTime.of(2016, 2, 4, 12, 00);
-
-		final OrderState orderState = OrderState.EXECUTED;
 		final RoomType roomType = RoomType.AMBASSADOR;
 		
+		final OrderGeneralVO createOrderGeneralVO = new OrderGeneralVO("1234567890", "12345678", "thisHotel", 
+				"nanjing", expectExecuteTime, expectLeaveTime);
+		final OrderVO createOrderVO = new OrderVO(createOrderGeneralVO, 250, roomType, 1, 2, "charles", "15012345678", "no");
 		
-		final OrderPO orderPO = new OrderPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200, 200,
-				createTime, checkInTime, checkOutTime, expectExecuteTime, expectLeaveTime, orderState, 
-				roomType, 2, "301", 2, "zhangsan", "13554321234", "no","good");
+		final OrderPO orderPO = new OrderPO(createOrderVO);
 		
 		try {
 			assertEquals(ResultMessage.SUCCESS, driver.orderDataService.createOrder(orderPO));
@@ -66,7 +96,7 @@ public class OrderDataService_DriverTest {
 	 * test interface executeOrder
 	 */
 	@Test
-	public void test2() {
+	public void test3() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -86,7 +116,7 @@ public class OrderDataService_DriverTest {
 	 * test interface undoAbnormalOrder
 	 */
 	@Test
-	public void test3() {
+	public void test4() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -106,7 +136,7 @@ public class OrderDataService_DriverTest {
 	 * test interface undoNormalOrder
 	 */
 	@Test
-	public void test4() {
+	public void test5() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -123,10 +153,10 @@ public class OrderDataService_DriverTest {
 	 * @lastChangedBy charles
 	 * @updateTime 2016/11/27
 	 * 
-	 * test interface getOrderDetails
+	 * test interface getOrderDetail
 	 */
 	@Test
-	public void test5() {
+	public void test6() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		final OrderPO orderPO;
@@ -168,7 +198,7 @@ public class OrderDataService_DriverTest {
 	 * test interface getAllGuestOrderGeneral
 	 */
 	@Test
-	public void test6() {
+	public void test7() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -200,7 +230,7 @@ public class OrderDataService_DriverTest {
 	 * test interface getAllHotelOrderGeneral
 	 */
 	@Test
-	public void test7() {
+	public void test8() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -232,7 +262,7 @@ public class OrderDataService_DriverTest {
 	 * test interface getAllAbnormalOrderGeneral(LocalDate date)
 	 */
 	@Test
-	public void test8() {
+	public void test9() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -264,7 +294,7 @@ public class OrderDataService_DriverTest {
 	 * test interface getAllAbnormalOrderGeneral()
 	 */
 	@Test
-	public void test9() {
+	public void test10() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -293,10 +323,157 @@ public class OrderDataService_DriverTest {
 	 * @lastChangedBy charles
 	 * @updateTime 2016/11/27
 	 * 
+	 * test interface getAllUnexecutedOrderGeneral
+	 */
+	@Test
+	public void test11() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		final List<OrderGeneralPO> orderGeneralPOs;
+		try {
+			orderGeneralPOs = driver.orderDataService.getAllUnexecutedOrderGeneral(LocalDate.of(2016, 2, 3));
+			
+			final OrderGeneralPO orderGeneralPO = orderGeneralPOs.get(0);	
+			
+			assertEquals("123456789012", orderGeneralPO.getOrderID());
+			assertEquals("1234567890", orderGeneralPO.getGuestID());
+			assertEquals("12345678", orderGeneralPO.getHotelID());
+			assertEquals("thisHotel", orderGeneralPO.getHotelName());
+			assertEquals("address", orderGeneralPO.getHotelAddress());
+			assertEquals(200, orderGeneralPO.getPrice(), 0);
+			assertEquals(LocalDateTime.of(2016, 2, 3, 14, 0), orderGeneralPO.getExpectExecuteTime());
+			assertEquals(LocalDateTime.of(2016, 2, 4, 12, 0), orderGeneralPO.getExpectLeaveTime());
+			assertEquals(OrderState.UNEXECUTED, orderGeneralPO.getState());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface updateCheckIn
+	 */
+	@Test
+	public void test12() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		ResultMessage message = ResultMessage.CHECK_IN_FAILURE;
+		try {
+			final LocalDateTime checkInTime = LocalDateTime.of(2016, 2, 3, 11, 23);
+			final LocalDateTime expectLeaveTime = LocalDateTime.of(2016, 2, 4, 12, 00);
+			final CheckInPO checkInPO = new CheckInPO("123456789012", "306", checkInTime, expectLeaveTime); 
+			
+			message = driver.orderDataService.updateCheckIn(checkInPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(ResultMessage.CHECK_IN_SUCCESS, message);
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface updateCheckOut
+	 */
+	@Test
+	public void test13() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		ResultMessage message = ResultMessage.CHECK_OUT_FAILURE;
+		try {
+			final LocalDateTime checkOutTime = LocalDateTime.of(2016, 2, 4, 10, 58);
+			final CheckOutPO checkOutPO = new CheckOutPO("123456789012", checkOutTime); 
+			
+			message = driver.orderDataService.updateCheckOut(checkOutPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(ResultMessage.CHECK_OUT_SUCCESS, message);
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface addEvaluation
+	 */
+	@Test
+	public void test14() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		ResultMessage message = ResultMessage.UPDATE_EVALUATION_FAILURE;
+		try {
+			final GuestEvaluationPO guestEvaluationPO = new GuestEvaluationPO("123456789012", 4.5, "good");
+			message = driver.orderDataService.addEvaluation(guestEvaluationPO);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		assertEquals(ResultMessage.UPDATE_EVALUATION_SUCCESS, message);
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface getEvaluations
+	 */
+	@Test
+	public void test15() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		final List<HotelEvaluationPO> hotelEvaluationPOs;
+		try {
+			hotelEvaluationPOs = driver.orderDataService.getEvaluations("12345678");
+			
+			final HotelEvaluationPO hotelEvaluationPO1 = hotelEvaluationPOs.get(0);
+			final HotelEvaluationPO hotelEvaluationPO2 = hotelEvaluationPOs.get(1);
+			final HotelEvaluationPO hotelEvaluationPO3 = hotelEvaluationPOs.get(2);
+			
+			assertEquals("1234567890", hotelEvaluationPO1.getGuestID());
+			assertEquals(LocalDate.of(2016, 11, 21), hotelEvaluationPO1.getCheckInDate());
+			assertEquals(4.5, hotelEvaluationPO1.getScore(), 0);
+			assertEquals("very good", hotelEvaluationPO1.getComment());
+			
+			assertEquals("1234567891", hotelEvaluationPO2.getGuestID());
+			assertEquals(LocalDate.of(2016, 11, 21), hotelEvaluationPO2.getCheckInDate());
+			assertEquals(4.5, hotelEvaluationPO2.getScore(), 0);
+			assertEquals("good", hotelEvaluationPO2.getComment());
+			
+			assertEquals("1234567891", hotelEvaluationPO3.getGuestID());
+			assertEquals(LocalDate.of(2016, 11, 23), hotelEvaluationPO3.getCheckInDate());
+			assertEquals(4.3, hotelEvaluationPO3.getScore(), 0);
+			assertEquals("ok", hotelEvaluationPO3.getComment());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/11/27
+	 * 
 	 * test interface getBookedHotels
 	 */
 	@Test
-	public void test10() {
+	public void test16() {
 		final OrderDataService_Stub stub = new OrderDataService_Stub();
 		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
 		
@@ -310,5 +487,26 @@ public class OrderDataService_DriverTest {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * 
+	 * test interface getOrderState
+	 */
+	@Test
+	public void test17() {
+		final OrderDataService_Stub stub = new OrderDataService_Stub();
+		final OrderDataService_Driver driver = new OrderDataService_Driver(stub);
+		
+		OrderState orderState = null;
+		try {
+			orderState = driver.orderDataService.getOrderState("1234567890", "12345678");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		assertEquals(OrderState.COMMENTED, orderState);
 	}
 }
--- CommonHub/src/main/java/dataService/orderDataService/OrderDataService_Stub.java
+++ CommonHub/src/main/java/dataService/orderDataService/OrderDataService_Stub.java
@@ -20,11 +20,24 @@ import utilities.RoomType;
  * 
  * @author cuihua
  * lastChangedBy charles
- * updateTime 2016/12/4
+ * updateTime 2016/12/5
  *
  */
 public class OrderDataService_Stub implements OrderDataService {
 
+	
+	/**
+	 * 
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/4
+	 * @param orderPO 从客户界面层传下来的Order载体
+	 * @return 若客户创建此订单，需要付的款项
+	 * @throws RemoteException RMI
+	 */
+	public double getTempPrice(OrderPO orderPO) throws RemoteException {
+		return 200;
+	}
 	
 	/**
 	 * 
@@ -35,7 +48,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 客户是否成功创建此订单
 	 * @throws RemoteException RMI
 	 */
-	public ResultMessage createOrder(final OrderPO order) {
+	public ResultMessage createOrder(final OrderPO order) throws RemoteException {
 		return ResultMessage.SUCCESS;
 	}
 
@@ -48,7 +61,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 酒店工作人员是否成功执行此订单
 	 * @throws RemoteException RMI
 	 */
-	public ResultMessage executeOrder(final String orderID) {
+	public ResultMessage executeOrder(final String orderID) throws RemoteException {
 		return ResultMessage.SUCCESS;
 	}
 
@@ -61,7 +74,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 网站营销人员是否成功撤销此异常订单
 	 * @throws RemoteException RMI
 	 */
-	public ResultMessage undoAbnormalOrder(final String orderID) {
+	public ResultMessage undoAbnormalOrder(final String orderID) throws RemoteException {
 		return ResultMessage.SUCCESS;
 	}
 
@@ -74,7 +87,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 客户是否成功撤销此正常订单
 	 * @throws RemoteException RMI
 	 */
-	public ResultMessage undoNormalOrder(final String orderID) {
+	public ResultMessage undoNormalOrder(final String orderID) throws RemoteException {
 		return ResultMessage.SUCCESS;
 	}
 	
@@ -87,7 +100,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 此被需要订单的详情载体
 	 * @throws RemoteException RMI
 	 */
-	public OrderPO getOrderDetail(final String orderID) {
+	public OrderPO getOrderDetail(final String orderID) throws RemoteException {
 		final LocalDateTime createTime = LocalDateTime.of(2016, 2, 2, 18, 20);
 		final LocalDateTime checkInTime = LocalDateTime.of(2016, 2, 3, 11, 23);
 		final LocalDateTime checkOutTime = LocalDateTime.of(2016, 2, 4, 10, 58);
@@ -112,7 +125,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 客户个人所有订单
 	 * @throws RemoteException RMI
 	 */
-	public List<OrderGeneralPO> getAllGuestOrderGeneral(final String guestID) {
+	public List<OrderGeneralPO> getAllGuestOrderGeneral(final String guestID) throws RemoteException {
 		final List<OrderGeneralPO> orderGenerals = new ArrayList<OrderGeneralPO>();
 		orderGenerals.add(new OrderGeneralPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200,
 				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.EXECUTED));
@@ -128,7 +141,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 此酒店所有的所有订单
 	 * @throws RemoteException RMI
 	 */
-	public List<OrderGeneralPO> getAllHotelOrderGeneral(final String hotelID) {
+	public List<OrderGeneralPO> getAllHotelOrderGeneral(final String hotelID) throws RemoteException {
 		final List<OrderGeneralPO> orderGenerals = new ArrayList<OrderGeneralPO>();
 		orderGenerals.add(new OrderGeneralPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200,
 				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.EXECUTED));
@@ -144,7 +157,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 网站营销人员需要查看的当天所有的异常订单
 	 * @throws RemoteException RMI
 	 */
-	public List<OrderGeneralPO> getAllAbnormalOrderGeneral(final LocalDate date) {
+	public List<OrderGeneralPO> getAllAbnormalOrderGeneral(final LocalDate date) throws RemoteException {
 		final List<OrderGeneralPO> orderGenerals = new ArrayList<OrderGeneralPO>();
 		orderGenerals.add(new OrderGeneralPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200,
 				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.EXECUTED));
@@ -159,7 +172,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @return 网站营销人员需要查看的所有的异常订单，按倒序排列
 	 * @throws RemoteException RMI
 	 */
-	public List<OrderGeneralPO> getAllAbnormalOrderGeneral() {
+	public List<OrderGeneralPO> getAllAbnormalOrderGeneral() throws RemoteException {
 		final List<OrderGeneralPO> orderGenerals = new ArrayList<OrderGeneralPO>();
 		orderGenerals.add(new OrderGeneralPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200,
 				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.EXECUTED));
@@ -170,7 +183,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	/**
 	 * @author charles
 	 * @lastChangedBy charles
-	 * @updateTime 2016/11/29
+	 * @updateTime 2016/12/5
 	 * @param date 网站营销人员查看未执行订单时输入的指定日期
 	 * @return 网站营销人员需要查看的当天所有的未执行订单
 	 * @throws RemoteException RMI
@@ -179,7 +192,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	public List<OrderGeneralPO> getAllUnexecutedOrderGeneral(final LocalDate date) throws RemoteException {
 		final List<OrderGeneralPO> orderGenerals = new ArrayList<OrderGeneralPO>();
 		orderGenerals.add(new OrderGeneralPO("123456789012", "1234567890", "12345678", "thisHotel", "address", 200,
-				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.EXECUTED));
+				LocalDateTime.of(2016, 2, 3, 14, 0), LocalDateTime.of(2016, 2, 4, 12, 0), OrderState.UNEXECUTED));
 		return orderGenerals;
 	}
 	
@@ -188,22 +201,24 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @author charles
 	 * @lastChangedBy charles
 	 * @updateTime 2016/12/4
-	 * @param checkInVO 酒店工作人员更新订单入住信息
+	 * @param checkInPO 酒店工作人员更新订单入住信息
 	 * @return 是否成功更新
+	 * @throws RemoteException RMI
 	 */
-	public ResultMessage updateCheckIn (CheckInPO checkInPO) throws RemoteException {
-		return ResultMessage.SUCCESS;
+	public ResultMessage updateCheckIn(CheckInPO checkInPO) throws RemoteException {
+		return ResultMessage.CHECK_IN_SUCCESS;
 	}
 
 	/**
 	 * @author charles
 	 * @lastChangedBy charles
 	 * @updateTime 2016/12/4
-	 * @param checkInVO 酒店工作人员更新订单退房信息
+	 * @param checkInPO 酒店工作人员更新订单退房信息
 	 * @return 是否成功更新
+	 * @throws RemoteException RMI
 	 */
 	public ResultMessage updateCheckOut (CheckOutPO checkOutPO) throws RemoteException {
-		return ResultMessage.SUCCESS;
+		return ResultMessage.CHECK_OUT_SUCCESS;
 	}
 	
 	
@@ -211,7 +226,7 @@ public class OrderDataService_Stub implements OrderDataService {
 	 * @author charles
 	 * @lastChangedBy charles
 	 * @updateTime 2016/12/4
-	 * @param evaluationVO 客户评价单个订单时产生的订单
+	 * @param evaluationPO 客户评价单个订单时产生的订单
 	 * @return 客户是否成功评价该订单
 	 * @throws RemoteException RMI
 	 */
@@ -232,7 +247,7 @@ public class OrderDataService_Stub implements OrderDataService {
 		List<HotelEvaluationPO> result = new ArrayList<HotelEvaluationPO>();
 		result.add(new HotelEvaluationPO("1234567890", LocalDate.of(2016, 11, 21), 4.5, "very good"));
 		result.add(new HotelEvaluationPO("1234567891", LocalDate.of(2016, 11, 21), 4.5, "good"));
-		result.add(new HotelEvaluationPO("1234567891", LocalDate.of(2016, 11, 23), 4.5, "ok"));
+		result.add(new HotelEvaluationPO("1234567891", LocalDate.of(2016, 11, 23), 4.3, "ok"));
 		return result;
 	}
 	
@@ -253,4 +268,16 @@ public class OrderDataService_Stub implements OrderDataService {
 		return bookedHotels;
 	}
 
+	/**
+	 * @author charles
+	 * @lastChangedBy charles
+	 * @updateTime 2016/12/5
+	 * @param guestID 此客户的客户编号
+	 * @param hotelID 此客户相对的酒店编号
+	 * @return 此客户在此相应酒店预定过的订单状态
+	 * @throws RemoteException RMI
+	 */
+	public OrderState getOrderState(String guestID, String hotelID) throws RemoteException {
+		return OrderState.COMMENTED;
+	}
 }
