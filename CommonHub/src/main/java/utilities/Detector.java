41ef2d323a2e4c732928b63193ddebe401c965bd
Byron Dong <2104869365@qq.com>
2016-12-06 15:17:48 +0800

    删除符号检查器

--- CommonHub/src/main/java/utilities/Detector.java
+++ CommonHub/src/main/java/utilities/Detector.java
@@ -5,16 +5,52 @@ import java.util.regex.Pattern;
 
 public class Detector {
 
+	private String expression; //存放正则表达式
 	
-	public static boolean idDetector(String express, int length){
+	public static void main(String []args){
+		Detector a= new Detector();
+		System.out.println(a.infoDetector("董金玉gigudg123456"));
+	} 
+	
+	public  boolean idDetector(String express, int length){
 		
-		String expression = "[0-9]{"+String.valueOf(length)+"}";
+		expression = "[0-9]{"+String.valueOf(length)+"}";
+		
+		return this.getResultOfDetector(express); //得到与指定长度匹配的数字
+	}
+	
+	public  boolean passwordDetector(String express){
+		
+		expression = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$";
+		
+		return this.getResultOfDetector(express); //密码必须是数字与字母的组合
+	}
+	
+	public boolean phoneDetector(String express){
+		expression = "[0-9]{11}";
+		
+		return this.getResultOfDetector(express);
+	}
+	
+	public boolean infoDetector(String express){ //统一检测填写信息内容中的不合法标识符
+		expression = "^[\u4e00-\u9fa5_a-zA-Z0-9]+$";
+		
+		return this.getResultOfDetector(express);
+	}
+	
+	public boolean discoutDetector(String express){ //折扣只允许单数字
+		expression = "[0-9]{1}";
+		
+		return this.getResultOfDetector(express);
+	}
+	
+	private boolean getResultOfDetector(String express){
 		
 		Pattern pattern = Pattern.compile(expression);
 		Matcher matcher = pattern.matcher(express);
-		boolean result = matcher.matches(); //当条件满足时，将返回true，否则返回false
+		boolean result = matcher.matches(); 
 		
-		return result;
+		return result; //得到匹配正则表达式的结果
 	}
 	
 }
