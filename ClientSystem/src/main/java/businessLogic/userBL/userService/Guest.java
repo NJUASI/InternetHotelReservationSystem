06fea310c714388eef259da2252ff9efa7cf17d3
Byron Dong <2104869365@qq.com>
2016-12-09 23:43:59 +0800

    为user部分实现加密与解密功能，大家可以为自己应该加密解密的信息添加了

--- ClientSystem/src/main/java/businessLogic/userBL/userService/Guest.java
+++ ClientSystem/src/main/java/businessLogic/userBL/userService/Guest.java
@@ -16,13 +16,10 @@ import vo.UserVO;
 
 /**
  * 
- * @author Byron Dong
- * lastChangedBy Byron Dong
- * updateTime 2016/11/27
+ * @author Byron Dong lastChangedBy Byron Dong updateTime 2016/11/27
  *
  */
-public class Guest implements UserService, GuestCreditService{
-
+public class Guest implements UserService, GuestCreditService {
 
 	public static int IDLength = 10; // 客户的ID长度为10
 
@@ -31,11 +28,11 @@ public class Guest implements UserService, GuestCreditService{
 	/**
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
-	 * @updateTime 2016/11/27
-	 * 构造函数，初始化成员变量
+	 * @updateTime 2016/11/27 构造函数，初始化成员变量
 	 */
 	public Guest() {
-//		guestDataService = ClientRemoteHelper.getInstance().getGuestDataService();
+		// guestDataService =
+		// ClientRemoteHelper.getInstance().getGuestDataService();
 		try {
 			guestDataService = new GuestDataService_Stub();
 		} catch (RemoteException e) {
@@ -47,13 +44,14 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/12/9
-	 * @param newUserVO 从userDoMain传下来的userInfo载体
+	 * @param newUserVO
+	 *            从userDoMain传下来的userInfo载体
 	 * @return ResultMessage 用户是否成功添加客户信息
 	 */
 	public UserVO add(UserVO newUserVO) {
 
 		try {
-			GuestPO guestPO = this.encrypt(convert(newUserVO));
+			GuestPO guestPO = convert(newUserVO);
 			return convert(guestDataService.add(guestPO));
 		} catch (RemoteException e) {
 			e.printStackTrace();
@@ -65,17 +63,20 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/27
-	 * @param newUserVO 从userDoMain传下来的userInfo载体
+	 * @param newUserVO
+	 *            从userDoMain传下来的userInfo载体
 	 * @return ResultMessage 用户是否成功修改客户信息
 	 */
 	public ResultMessage modify(UserVO userVO) {
 
 		ResultMessage msg = ResultMessage.USER_INFO_UPDATE_FAILURE;
 
-		if(!this.hasGuest(userVO.userID)){return msg;} //不存在ID对应项
+		if (!this.hasGuest(userVO.userID)) {
+			return msg;
+		} // 不存在ID对应项
 
 		try {
-			GuestPO guestPO = this.encrypt(convert(userVO));
+			GuestPO guestPO = convert(userVO);
 			msg = guestDataService.modify(guestPO);
 		} catch (RemoteException e) {
 			e.printStackTrace();
@@ -87,13 +88,14 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/27
-	 * @param userVO 从userDoMain传下来的用户ID
+	 * @param userVO
+	 *            从userDoMain传下来的用户ID
 	 * @return UserVO 单一guestInfo载体
 	 */
 	public UserVO getSingle(String userID) {
 
 		try {
-			return this.convert(this.decode(guestDataService.getSingleGuest(userID)));
+			return this.convert(guestDataService.getSingleGuest(userID));
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
@@ -104,7 +106,7 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/27
-	 * @param  
+	 * @param
 	 * @return List<UserVO> 指定用户类型的所有guestInfo载体
 	 */
 	public List<UserVO> getAll() {
@@ -120,19 +122,22 @@ public class Guest implements UserService, GuestCreditService{
 	/**
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
-	 * @updateTime 2016/11/27
-	 * @param  userID 从userDoMain传下来的指定用户ID
+	 * @updateTime 2016/12/9
+	 * @param userID
+	 *            从userDoMain传下来的指定用户ID
 	 * @return String 指定用户 的登录信息
 	 */
 	public String getLogInInfo(String userID) {
 
-		if(!this.hasGuest(userID)){return null;} //指定的客户不存在，返回null
+		if (!this.hasGuest(userID)) {
+			return null;
+		} // 指定的客户不存在，返回null
 
 		try {
-			return guestDataService.getSingleGuest(userID).getPassword();
+			return convert(guestDataService.getSingleGuest(userID)).password;
 		} catch (RemoteException e) {
 			e.printStackTrace();
-			return null; //出现异常返回null
+			return null; // 出现异常返回null
 		}
 	}
 
@@ -140,7 +145,8 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Harvey Gong
 	 * @updateTime 2016/12/5
-	 * @param guestID, creditNum从userDoMain传下来的指定客户ID和需修改的信用值
+	 * @param guestID,
+	 *            creditNum从userDoMain传下来的指定客户ID和需修改的信用值
 	 * @return ResultMessage 信用值是否添加成功
 	 */
 	public ResultMessage modifyCredit(String guestID, double creditNum) {
@@ -159,7 +165,8 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/27
-	 * @param  IDLength 用户ID长度
+	 * @param IDLength
+	 *            用户ID长度
 	 * @return boolean 判断指定用户是否为客户类型
 	 */
 	public boolean isGuest(int length) {
@@ -174,69 +181,75 @@ public class Guest implements UserService, GuestCreditService{
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/28
-	 * @param  guestPO 来自本类guestInfo载体
+	 * @param guestPO
+	 *            来自本类guestInfo载体
 	 * @return UserVO userInfo载体
 	 */
 	private UserVO convert(GuestPO guestPO) {
-		if(guestPO==null){return null;}
-		return new GuestVO(guestPO);
+		if (guestPO == null) {
+			return null;
+		}
+		return new GuestVO(this.decode(guestPO));
 	}
 
 	/**
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/28
-	 * @param  userPO 来自本类userInfo载体
+	 * @param userPO
+	 *            来自本类userInfo载体
 	 * @return GuestVO guestInfo载体
 	 */
 	private GuestPO convert(UserVO userVO) {
-		if(userVO==null){return null;}
-		return new GuestPO((GuestVO) userVO);
+		if (userVO == null) {
+			return null;
+		}
+		return this.encrypt(new GuestPO((GuestVO) userVO));
 	}
 
 	/**
 	 * @author Byron Dong
 	 * @lastChangedBy Byron Dong
 	 * @updateTime 2016/11/28
-	 * @param  List<GuestPO> 来自本类所有guestInfo载体
+	 * @param List<GuestPO>
+	 *            来自本类所有guestInfo载体
 	 * @return List<UserVO> 所有对应的userInfo载体
 	 */
 	private List<UserVO> convert(List<GuestPO> list) {
 		List<UserVO> result = new ArrayList<UserVO>();
 		for (int i = 0; i < list.size(); i++) {
-			result.add(new GuestVO(list.get(i)));
+			result.add(new GuestVO(this.decode(list.get(i))));
 		}
 		return result;
 	}
 
-	public boolean hasGuest(String guestID) { //放在后面，该方法一般只在本类使用，可以等同private,只有member会用到
+	public boolean hasGuest(String guestID) { // 放在后面，该方法一般只在本类使用，可以等同private,只有member会用到
 		UserVO guestVO = this.getSingle(guestID);
 
-		if(guestVO==null){
+		if (guestVO == null) {
 			return false;
-		}
-		else{
+		} else {
 			return true;
 		}
 	}
-	
-	private GuestPO encrypt(GuestPO guestPO){
+
+	private GuestPO encrypt(GuestPO guestPO) {
 		Ciphertext encrpyt = new Ciphertext();
-		
+
 		guestPO.setName(encrpyt.encrypt(guestPO.getName()));
 		guestPO.setPassword(encrpyt.encrypt(guestPO.getPassword()));
 		guestPO.setPhone(encrpyt.encrypt(guestPO.getPhone()));
-		
+
 		return guestPO;
 	}
-	
-	private GuestPO decode(GuestPO guestPO){
+
+	private GuestPO decode(GuestPO guestPO) {
 		Ciphertext decode = new Ciphertext();
-		
+
 		guestPO.setName(decode.decode(guestPO.getName()));
 		guestPO.setPassword(decode.decode(guestPO.getPassword()));
 		guestPO.setPhone(decode.decode(guestPO.getPhone()));
-		
+
 		return guestPO;
 	}
 
