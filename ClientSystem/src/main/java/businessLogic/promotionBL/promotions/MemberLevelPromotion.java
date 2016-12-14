294d3df07c818b8765427b15d086cd854ccb7e86
Monkey Gong <335931662@qq.com>
2016-12-14 22:40:57 +0800

    不允许重复添加普通策略和客房房型

--- ClientSystem/src/main/java/businessLogic/promotionBL/promotions/MemberLevelPromotion.java
+++ ClientSystem/src/main/java/businessLogic/promotionBL/promotions/MemberLevelPromotion.java
@@ -5,16 +5,8 @@ import exception.verificationException.UserInexistException;
 
 public class MemberLevelPromotion {
 
-	public MemberLevelPromotion() {
-
-	}
-
 	public double getDiscount(String guestID) throws UserInexistException{
 		Market market = new Market();
-		/*
-		 * TODO djy 提供一个返回折扣的接口，我把客户的id传给你，你判断是否是会员、会员等级，再获取对应的折扣
-		 * 然后把折扣返回给我
-		 */
 		return 1;
 	}
 
